bb82c001a756946cc56d10d06258215cbe1493b7
NicolasMC94 <mcnicolas.94@gmail.com>
2022-08-01 18:06:11 +0000

    Corrección de Jesús

--- src/app.js
+++ src/app.js
@@ -1,19 +1,29 @@
 window.onload = function domainName() {
-  let domainName = [];
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
   let domain = [".es", ".com"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < domain.length; l++) {
-          domainName.push(" " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+  function generateDomainNames(pronoun, adj, noun, domain) {
+    let domainsName = [];
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < domain.length; l++) {
+            domainsName.push(" " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+          }
         }
       }
     }
+    return domainsName;
   }
-  console.log(domainName);
-  document.getElementById("domain").innerHTML = domainName;
+  console.log(generateDomainNames(pronoun, adj, noun, domain));
+  document.getElementById("domain").innerHTML = generateDomainNames(
+    pronoun,
+    adj,
+    noun,
+    domain
+  );
+  domain.push(".net");
+  generateDomainNames(pronoun, adj, noun, domain);
 };
